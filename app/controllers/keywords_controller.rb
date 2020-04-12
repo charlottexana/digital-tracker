e937c93deffdbe1f988a998783386aca032ae060
Charlotte Waller <charlotteawaller@gmail.com>
2020-04-12 16:47:31 +0100

    Worked on backend models

---
+++ app/controllers/keywords_controller.rb
@@ -0,0 +1,34 @@
+class KeywordsController < ApplicationController
+  def index
+    @keywords = Keyword.all
+  end
+
+  def show
+    @keyword = Keyword.find(params[:id])
+  end
+
+  def new
+    @keyword = Keyword.new
+  end
+
+  def create
+    @keyword = Keyword.new(keyword_params)
+    @keyword.save
+  end
+
+  def edit
+    @keyword = Keyword.find(params[:id])
+  end
+
+  def update
+    @keyword = Keyword.find(params[:id])
+    @keyword.update(keyword_params)
+  end
+
+  private
+
+  def keyword_params
+    params.require(:keyword).permit(:name, :date, :current_rank)
+  end
+
+end
