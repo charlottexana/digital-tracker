262a0b092b36b24aa2f11247fef76bb3a349d571
Charlotte Waller <charlotteawaller@gmail.com>
2020-04-13 19:51:00 +0100

    Got API working for mobile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -45,7 +45,28 @@ fetch('https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7
   .then(response => response.json())
   .then((data) => {
     const newRanking = (data.organic.map(position => {
-      return position.title
+      return position.url
+    }));
+
+  for (var i = 0; i < newRanking.length; i++) {
+      const title = newRanking[i];
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(title ));
+      myKeyword.appendChild(li);
+  }}
+  );
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+
+const myKeyword = document.querySelector('#mobile');
+// debugger
+
+fetch('https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20lifeguard&device=mobile&gl=GB&hl=en&location=United%20Kingdom')
+  .then(response => response.json())
+  .then((data) => {
+    const newRanking = (data.organic.map(position => {
+      return position.url
     }));
 
   for (var i = 0; i < newRanking.length; i++) {
