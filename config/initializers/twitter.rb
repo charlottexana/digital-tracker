5f3acae16d3f2aa48dd8b617b2569ef253f70b2b
Charlotte Waller <charlotteawaller@gmail.com>
2020-04-19 15:55:42 +0100

    Twitter stopped working

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,11 +1,19 @@
 require 'twitter'
 # require 'yaml'
 
-$client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "ZBakGB87nijFCZMc97rG4fxz2"
-  config.consumer_secret     = "NkvsisbmLvxqwqVnxduHyyOMsV5HT33Qg19SWl3C9ekVHj0oj2"
-  config.access_token        = "1168473279520301056-KuReMUODcm8I2YstZQijvWbaYJnseP"
-  config.access_token_secret = "IeQD5e47x1BTgoPDOzoJnECqtH2zf17gnbvrzws0dAzwN"
-end
+# $client = Twitter::REST::Client.new do |config|
+#   config.consumer_key        = "twitter_consumer_key"
+#   config.consumer_secret     = "twitter_consumer_key_secret"
+#   config.access_token        = "twitter_access_token"
+#   config.access_token_secret = "twitter_access_token_secret"
+# end
 
 # tweets = client.user_timeline('LLLifeguard', count: 10)
+
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_SECRET']
+end
