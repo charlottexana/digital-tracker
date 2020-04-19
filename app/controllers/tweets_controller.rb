bfcc19b275cebbe8dd8f20e76fb19d6aca15d36f
Charlotte Waller <charlotteawaller@gmail.com>
2020-04-19 17:08:33 +0100

    Displayed some basic Twitter stats

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,5 +3,6 @@ class TweetsController < ApplicationController
     username = 'LLLifeguard'
     options = { :count => 5, :include_rts => true }
     @tweets = $client.user_timeline(username, options)
+    @followers = $client.user
   end
 end
