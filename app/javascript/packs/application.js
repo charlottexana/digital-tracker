883df5844bda6bd56f60752f4d25abf65e21baeb
Charlotte Waller <charlotteawaller@gmail.com>
2020-04-19 11:53:25 +0100

    Made app.js file structure more sensible

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,48 +34,5 @@ document.addEventListener('turbolinks:load', () => {
   initUpdateNavbarOnScroll();
 });
 
-// desktop
-
-document.addEventListener('DOMContentLoaded', function(){
-
-const myKeyword = document.querySelector('#keywords');
-// debugger
-
-fetch('https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20lifeguard&device=desktop&gl=GB&hl=en&location=United%20Kingdom')
-  .then(response => response.json())
-  .then((data) => {
-    const newRanking = (data.organic.map(position => {
-      return position.url
-    }));
-
-  for (var i = 0; i < newRanking.length; i++) {
-      const title = newRanking[i];
-      const li = document.createElement('li');
-      li.appendChild(document.createTextNode(title ));
-      myKeyword.appendChild(li);
-  }}
-  );
-});
-
-// mobile
-
-document.addEventListener('DOMContentLoaded', function(){
-
-const myKeyword = document.querySelector('#mobile');
-// debugger
-
-fetch('https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20lifeguard&device=mobile&gl=GB&hl=en&location=United%20Kingdom')
-  .then(response => response.json())
-  .then((data) => {
-    const newRanking = (data.organic.map(position => {
-      return position.url
-    }));
-
-  for (var i = 0; i < newRanking.length; i++) {
-      const title = newRanking[i];
-      const li = document.createElement('li');
-      li.appendChild(document.createTextNode(title ));
-      myKeyword.appendChild(li);
-  }}
-  );
-});
+import '../components/desktopresults';
+import '../components/mobileresults';
