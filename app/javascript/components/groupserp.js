4b14ccba53d0717ef881492d35cedb4b327d7e5e
Charlotte Waller <charlotteawaller@gmail.com>
2020-05-09 10:53:52 +0100

    Accessed items in batch code

--- app/javascript/components/groupserp.js
+++ app/javascript/components/groupserp.js
@@ -1,22 +1,37 @@
-// const request = require("request");
+const headers = {
+  'apikey': '700c9fe0-74fd-11ea-826c-03f7ed395182',
+  'content-type': 'application/json'
+};
 
-// const options = {
-//   method: "POST",
-//   url: "https://app.zenserp.com/api/v2/batches/search?&q=#{query}&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=20",
-//   qs: {
-//     "api_key": "700c9fe0-74fd-11ea-826c-03f7ed395182"
-//   },
-//   headers: {
-//     "content-type": "application/json"
-//   },
-//   body: {
-//     query: "landlord lifeguard"
-//   },
-//   json: true
-// };
+const options = {
+  "url": 'https://app.zenserp.com/api/v2/batches/',
+  "webhook_url": "http://localhost:3000/keywords",
+  "jobs": [
+        {
+            "q": "landlord lifeguard",
+        },
 
-// request(options, function (error, response, body) {
-//   if (error) throw new Error(error);
+        {
+            "keyword": [
+              "landlord lifeguard",
+              "landlord advice",
+              "landlord help",
+              "tenant referencing",
+              "should i sell my rental property"
+              ],
 
-//   console.log(body);
-// });
+              "type": "search"
+        }
+      ]
+};
+
+console.log(options.jobs[1]["keyword"][3]);
+
+// batchSerp = (options.jobs[1]["keyword"].forEach("keyword" => {
+//   debugger
+//   fetch('https://app.zenserp.com/api/v2/batches/');
+//   console.log("this worked");
+// }
+
+//   ));
+
