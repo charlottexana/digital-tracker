797333993ba2f34bd7ae12273eff1f48b0f07522
Charlotte Waller <charlotteawaller@gmail.com>
2020-05-11 14:10:34 +0100

    Dennis got serp feature working

--- app/javascript/components/groupserp.js
+++ app/javascript/components/groupserp.js
@@ -1,28 +1,54 @@
-// const options = {
-//   "url": 'https://app.zenserp.com/api/v2/batches/',
-//   'apikey': '700c9fe0-74fd-11ea-826c-03f7ed395182',
-//   'content-type': 'application/json',
-//   "webhook_url": "http://localhost:3000/keywords",
-//   "jobs": [
-//         {
-//             "q": "landlord lifeguard",
-//         },
+// curl -X POST \
+// https://app.zenserp.com/api/v1/batches \
+// -H 'Content-Type: application/json' \
+// -H 'apikey: 700c9fe0-74fd-11ea-826c-03f7ed395182' \
+// -d '{
+//         "webhook_url": "https://webhook.site",
+//         "jobs": [
+//             {
+//                 "q": "landlord lifeguard",
+//                 "custom_id":"customId123"
+//             },
+//             {
+//                 "product_id": "6900132173303686808",
+//                 "custom_id":"customId123",
+//                 "type": "search"
+//             },
+//             {
+//                 "keyword": [
+//                     "landlord advice",
+//                     "tenant referencing"
+//                 ],
+//                 "type": "trends"
+//             }
+//         ]
+//     }'
 
-//         {
-//             "keyword": [
-//               "landlord lifeguard",
-//               "landlord advice",
-//               "landlord help",
-//               "tenant referencing",
-//               "should i sell my rental property"
-//               ],
+const options = {
+  "url": "https://app.zenserp.com/api/v2/batches/",
+  "apikey": "700c9fe0-74fd-11ea-826c-03f7ed395182",
+  "content-type": "application/json",
+  "webhook_url": "http://localhost:3000/keywords",
+  "jobs": [
+        {
+            "q": "landlord lifeguard",
+        },
 
-//               "type": "search"
-//         }
-//       ]
-// };
+        {
+            "keyword": [
+              "landlord lifeguard",
+              "landlord advice",
+              "landlord help",
+              "tenant referencing",
+              "should i sell my rental property"
+              ],
 
-// console.log(options.jobs[1]["keyword"][3]);
+              "type": "search",
+        },
+      ]
+};
+
+console.log(options.jobs[1]["keyword"][3]);
 
 // batchSerp = (options.jobs[1]["keyword"].forEach("keyword" => {
 //   debugger
--- app/javascript/components/serps.js
+++ app/javascript/components/serps.js
@@ -3,22 +3,28 @@ document.addEventListener('DOMContentLoaded', function(){
 
     if(serp) {
 
-    fetch('https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord advice&device=desktop&gl=GB&hl=en&location=United Kingdom&num=20')
-      .then(response => response.json())
-      .then((data) => {
-        const newPosition = (data.organic.forEach(position => {
-          if (position.url.includes("gov.uk")) {
-            console.log("Your position is " + position.position);
-            console.log("You are in the top 100 for this term!");
-            serp.textContent = ("Your position is " + position.position + ". You are in the top 100 for this term!");
-          } else {
-            console.log("You are not in the top 100 - more SEO work is needed.");
-          };
-        }));
+    const urls = ["https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20advice&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=20", "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=should%20i%20sell%20my%20rental%20property&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=50"];
+    const promises = urls.map(url => fetch(url).then(response => response.json()));
+    // fetch('https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord advice&device=desktop&gl=GB&hl=en&location=United Kingdom&num=20')
+
+      Promise.all(promises)
+      // .then(response => response.json())
+      .then((results) => {
+        results.forEach((data, index) => {
+          console.log("current url", urls[index]);
+          const newPosition = (data.organic.forEach(position => {
+            if (position.url.includes("landlordlifeguard.co.uk")) {
+              console.log("Your position is " + position.position);
+              console.log("You are in the top 100 for this term!");
+              serp.textContent = ("Your position is " + position.position + ". You are in the top 100 for this term!");
+            } else {
+              console.log("You are not in the top 100 - more SEO work is needed.");
+            };
+          }));
+        })
       });
     };
 });
-
 
 
 
