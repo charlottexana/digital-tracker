b5815b9f9ec6214f825ad10936ff156a501b88a2
Charlotte Waller <charlotteawaller@gmail.com>
2020-05-16 09:45:21 +0100

    Cleaned up if statement in js

--- app/javascript/components/serps.js
+++ app/javascript/components/serps.js
@@ -15,11 +15,9 @@ document.addEventListener('DOMContentLoaded', function(){
           console.log("current url", fetches[index]);
           const newPosition = (data.organic.forEach(position => {
             if (position.url.includes("landlordlifeguard.co.uk")) {
-              console.log("Your position is " + position.position);
               console.log("You are in the top 100 for this term!");
               serp.textContent = ("Your position is " + position.position + ". You are in the top 100 for this term!");
-            } else {
-              console.log("You are not in the top 100 - more SEO work is needed.");
+              return("Your position is " + position.position + "You are in the top 100 for this term!");
             };
           }));
         });
