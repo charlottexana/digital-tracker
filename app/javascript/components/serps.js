74bbc2fd2cbd720892095cd09c3899c78182fb20
Charlotte Waller <charlotteawaller@gmail.com>
2020-05-24 20:09:42 +0100

    Display queries and results

--- app/javascript/components/serps.js
+++ app/javascript/components/serps.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     if(serp) {
 
     const fetches = ["https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20lifeguard&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=10",
-    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20advice&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=20",
+    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20advice&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=20",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=should%20i%20sell%20my%20rental%20property&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=20",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=what%20is%20tenant%20referencing&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=20",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=preparing%20your%20property%20to%20rent&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=50",
