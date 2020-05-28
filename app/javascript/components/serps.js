fe22551eb1e300d07aba383b1ca9d768c071362e
Charlotte Waller <charlotteawaller@gmail.com>
2020-05-28 19:11:13 +0100

    Ranking tool working a little better

--- app/javascript/components/serps.js
+++ app/javascript/components/serps.js
@@ -1,3 +1,4 @@
+console.log("Hello from digital tracker");
 document.addEventListener('DOMContentLoaded', function(){
   const serp = document.querySelector('#serp');
 
@@ -10,20 +11,19 @@ document.addEventListener('DOMContentLoaded', function(){
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=preparing%20your%20property%20to%20rent&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=cost%20of%20being%20a%20landlord&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20rights&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50",
-    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=end%20of%20tenancy%20checklist&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=50",
+    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=end%20of%20tenancy%20checklist&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=tenant%20reference%20check&device=desktop&gl=GB&hl=en&location=United%20Kingdom&&start=10&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=first%20time%20landlord&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20tax%20return&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20help&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=managing%20your%20rental%20property&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20checklist%20&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=5&num=50",
-    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=marketing%20your%20property&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=50",
+    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=marketing%20your%20property&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20tenant%20dispute&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50"];
     const promises = fetches.map(url => fetch(url).then(response => response.json()));
       Promise.all(promises)
       .then((results) => {
         results.forEach((data, index) => {
-          console.log("current url", fetches[index]);
           console.log("query", data.query.q);
           const query = (data.query.q);
           const li = document.createElement('li');
