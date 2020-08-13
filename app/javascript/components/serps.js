34446f631ae119a74d299eca6279b58673c3e3cc
Charlotte Waller <charlotteawaller@gmail.com>
2020-08-13 19:29:57 +0100

    Redoing ranking

--- app/javascript/components/serps.js
+++ app/javascript/components/serps.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function(){
     if(serp) {
 
     const fetches = ["https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20lifeguard&device=desktop&gl=GB&hl=en&location=United%20Kingdom&num=10",
-    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20advice&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=20",
+    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=tenancy%20checklist%20landlord%20lifeguard&device=desktop&gl=GB&hl=en&location=United%20Kingdom",
+    "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=landlord%20advice&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=10",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=should%20i%20sell%20my%20rental%20property&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=20",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=what%20is%20tenant%20referencing&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=20",
     "https://app.zenserp.com/api/v2/search?apikey=700c9fe0-74fd-11ea-826c-03f7ed395182&q=preparing%20your%20property%20to%20rent&device=desktop&gl=GB&hl=en&location=United%20Kingdom&start=10&num=50",
@@ -29,14 +30,14 @@ document.addEventListener('DOMContentLoaded', function(){
           const li = document.createElement('li');
           li.appendChild(document.createTextNode(query));
           serp.appendChild(li);
-          const newPosition = (data.organic.forEach(rank => {
-            if (rank.url.includes("landlordlifeguard.co.uk")) {
-              const text = query + ": " + rank.position;
-              const li = document.createElement('li');
-              li.appendChild(document.createTextNode(text));
-              serp.appendChild(li);
-            };
-          }));
+          // const newPosition = (data.organic.forEach(rank => {
+          //   if (rank.url.includes("landlordlifeguard.co.uk")) {
+          //     const text = query + ": " + rank.position;
+          //     const li = document.createElement('li');
+          //     li.appendChild(document.createTextNode(text));
+          //     serp.appendChild(li);
+          //   };
+          // }));
         });
       });
     };
