b305993af92d6f274962e7ac03058b3994c1a2b5
Charlotte Waller <charlotteawaller@gmail.com>
2020-08-13 18:48:39 +0100

    home model and migration for cookie

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_145309) do
+ActiveRecord::Schema.define(version: 2020_08_13_174721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
